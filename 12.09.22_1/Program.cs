d8258ade4d0dfbe1a3e308f6335d66fe79b39ae1
Egor <egorbrylev123@gmail.com>
2022-09-12 11:55:09 +0300

    12.09.22 complete

---
+++ 12.09.22_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("How many numbers you want to enter?");
+int arraycCount = int.Parse(Console.ReadLine());
+int[] myArray = new int[arraycCount];
+Console.WriteLine("Now please type those numbers one after another");
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.WriteLine($"Please enter {i} number");
+    myArray[i] = int.Parse(Console.ReadLine());
+
+}
+
+int[] result = Array.FindAll(myArray, i => i > 0);
+Console.WriteLine($"Number of numbers higher than 0 is:{result.Length}");
---
+++ 12.09.22_2/Program.cs
@@ -0,0 +1,14 @@
+﻿double b1, b2, k1, k2;
+
+Console.WriteLine("Write b1 coordinate");
+b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Write b2 coordinate");
+b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Write k1 coordinate");
+k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Write k2 coordinate");
+k2 = double.Parse(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+
+Console.WriteLine($"The result is: {x} ; {k1*x+b1}");
