da1b3944a2d142f099aa761110dc5cb4ccd72fd7
Egor <egorbrylev123@gmail.com>
2022-09-30 12:00:25 +0300

    Function Fill Array created

---
+++ Final_Program/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Please enter the size of the array");
+int size = int.Parse(Console.ReadLine());
+string[] myArray = new string[size];
+
+
+
+
+
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Enter {i} position in array");
+        array[i] = Console.ReadLine(); 
+    }
+}
+
