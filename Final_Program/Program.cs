16bf9b0f714c076de72fff810637c09e1ef4c61a
Egor <egorbrylev123@gmail.com>
2022-09-30 12:37:08 +0300

    Sort array method done

--- Final_Program/Program.cs
+++ Final_Program/Program.cs
@@ -1,12 +1,4 @@
-﻿Console.WriteLine("Please enter the size of the array");
-int size = int.Parse(Console.ReadLine());
-string[] myArray = new string[size];
-
-
-
-
-
-void FillArray(string[] array)
+﻿void FillArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
@@ -15,3 +7,48 @@ void FillArray(string[] array)
     }
 }
 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+ "  ");
+    }
+}
+
+
+void SortArray (string[] array)
+{
+    int NewArrayLength = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        int length = array[i].Length;
+        if (length > 0 && length < 4)
+        {
+            NewArrayLength += 1;
+        }
+
+    }
+    string[] TempArray = new string[NewArrayLength];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length > 0 && array[i].Length < 4)
+        {
+            TempArray[count] = array[i];
+            count += 1;
+        }
+
+    }
+    Console.Write("--->");
+    PrintArray(TempArray);
+
+}
+
+
+Console.WriteLine("Please enter the size of the array");
+int size = int.Parse(Console.ReadLine());
+string[] myArray = new string[size];
+
+FillArray(myArray);
+PrintArray(myArray);
+SortArray(myArray);
